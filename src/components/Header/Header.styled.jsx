1cf9d7a691a2d7c20096118f8024ae13e1ca2ab9
andrenaP <a0993100106@gmail.com>
2023-12-03 23:59:48 +0200

    add preaty

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -13,39 +13,18 @@ export const Navigation = styled.nav`
 
 export const StyledLink = styled(Link)`
   padding: 8px 16px;
-  color: #ffffff; /* White text color */
-  text-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff;
-  transition:
-    color 0.3s,
-    text-shadow 0.3s;
+  color: #000000; /* White text color */
 
   &:hover,
   &:focus {
     color: #00baff; /* Blue text color on hover/focus */
-    text-shadow:
-      0 0 5px #00baff,
-      0 0 10px #00baff,
-      0 0 20px #00baff,
-      0 0 40px #00baff,
-      0 0 80px #00baff;
   }
 `;
 
 export const IconWrapper = styled.svg`
   width: ${(props) => props.size || '12px'};
   height: ${(props) => props.size || '12px'};
-  fill: ${(props) => props.color || '#ffffff'};
+  fill: ${(props) => props.color || '#00000'};
   display: inline-block;
   margin-right: 5px;
-  box-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff;
 `;
